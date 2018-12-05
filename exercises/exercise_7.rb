82e7b49759d1a0c5f991936fb79bd0ba159d6e6f
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-05 20:31:45 +0000

    exercise 7 completed, last part of assignment

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "New store name:"
+new_store = Store.create(name: gets.chomp)
+puts new_store.errors.full_messages # this will retnr an error because the annual_revenue is required byt the validations in store.rb
+
+# The code below would work without throwing an error because both the store name and annual revenue are required inputs
+
+# print "Input new store name first, and then the annual revenue:"
+# new_store = Store.create(name: gets.chomp, annual_revenue: gets.chomp)
+# puts new_store.errors.full_messages
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, :inclusion => 40..200 # this syntax means between 40 and 200
+  validates :store_id, presence: true
+
 end
