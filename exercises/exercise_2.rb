010932b7fd1c11dedd2fc4fc72bf537f3cb9e9b2
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-05 18:00:40 +0000

    2nd exercise works

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.name = 'BryansCool'
+@store1.save
+
+# Store.find(1) returns the object for ID = 1
